07f41a9a73715771b00564f77423565ee72764fb
Chirag <chiragsonar1718@gmail.com>
2023-06-06 22:12:18 +0530

    feat: added navigation var for different pages to rener different buttons.

--- src/components/General/Navigation.jsx
+++ src/components/General/Navigation.jsx
@@ -11,13 +11,24 @@ const Navigation = () => {
     navigate("/dashboard");
   };
   return (
-    <div className="h-14 sticky top-0 flex items-center justify-between  bg-neutral-50 shadow-sm border-neutral-300- border-b-[1px]">
-      {window.location.pathname.includes("/dashboard") && (<div className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200" onClick={handleDashboard}>
-        Giga Growth Ventures 
-      </div>)}
-      {window.location.pathname.includes("/") && !window.location.pathname.includes("/dashboard") &&(<div className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200" onClick={handleHome}>
-        Giga Growth Ventures 
-      </div>)}
+    <div className="h-14 sticky top-0 flex items-center justify-between z-99  bg-neutral-50 shadow-sm border-neutral-300- border-b-[1px]">
+      {window.location.pathname.includes("/dashboard") && (
+        <div
+          className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200"
+          onClick={handleDashboard}
+        >
+          Giga Growth Ventures
+        </div>
+      )}
+      {window.location.pathname.includes("/") &&
+        !window.location.pathname.includes("/dashboard") && (
+          <div
+            className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200"
+            onClick={handleHome}
+          >
+            Giga Growth Ventures
+          </div>
+        )}
       <Sidebar />
     </div>
   );
