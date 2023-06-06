fdb29f48273732b587e77d0e932194fe962fa77b
Chirag <chiragsonar1718@gmail.com>
2023-06-06 19:52:57 +0530

    Feat: added home component.

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router-dom";
 // local imports
 import SignIn from "../components/Auth/SignIn";
 import Dashboard from "../components/Dashboard/Dashboard";
+import Home from "../components/General/Home";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<SignIn />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<SignIn />} />
       <Route exact path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
