a2f2b810367a6d24b2fc03e13527a8a5926fdff4
Chirag <chiragsonar1718@gmail.com>
2023-06-06 22:09:00 +0530

    Style: fixed css typos.

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SignIn = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col w-[250px] h-[400px] border-2 border-black rounded-lg">
-        <div className="flex flex-col justify-center items-center mt-5 text-sm">
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col w-[250px] h-[400px] md:w-[400px] border-2 shadow-md rounded-lg">
+        <div className="flex flex-col justify-center items-center mt-5 text-sm md:text-lg md:flex-row md:gap-2 font-montserrat">
           <span>Sign In with</span>
           <span>your Google Account</span>
         </div>
