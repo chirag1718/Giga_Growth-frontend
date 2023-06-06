1da93e235d76780c027320604c837f04ace43517
Chirag <chiragsonar1718@gmail.com>
2023-06-06 19:55:12 +0530

    Feat- added sidebar component and logo.

--- src/components/General/Navigation.jsx
+++ src/components/General/Navigation.jsx
@@ -11,7 +11,7 @@ const Navigation = () => {
     navigate("/dashboard");
   };
   return (
-    <div className="h-14 flex items-center justify-between  bg-neutral-50 shadow-sm border-neutral-300- border-b-[1px]">
+    <div className="h-14 sticky top-0 flex items-center justify-between  bg-neutral-50 shadow-sm border-neutral-300- border-b-[1px]">
       {window.location.pathname.includes("/dashboard") && (<div className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200" onClick={handleDashboard}>
         Giga Growth Ventures 
       </div>)}
