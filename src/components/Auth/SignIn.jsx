b587a07d3465e88d8581cc23c77825a10bbb2d2b
Chirag <chiragsonar1718@gmail.com>
2023-06-06 19:51:05 +0530

    Feat- added signin form

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col w-[250px] h-[400px] border-2 border-black rounded-lg">
+        <div className="flex flex-col justify-center items-center mt-5 text-sm">
+          <span>Sign In with</span>
+          <span>your Google Account</span>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
