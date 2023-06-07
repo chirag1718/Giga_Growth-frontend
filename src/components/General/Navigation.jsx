241092ce293e9d2feb2addeb4cdda3f873f60e6c
Chirag <chiragsonar1718@gmail.com>
2023-06-07 22:20:48 +0530

    feat: added conditional routing acc to user state.

--- src/components/General/Navigation.jsx
+++ src/components/General/Navigation.jsx
@@ -1,49 +1,14 @@
 import React from "react";
-import { UserAuth } from "../../context/AuthContext";
+import Sidebar from "./Sidebar";
 const Navigation = () => {
-  const { user, logOut } = UserAuth();
-  const { googleSignIn } = UserAuth();
-  const handleLogout = async () => {
-    try {
-      await logOut();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  const handleGoogleSignIn = async () => {
-    try {
-      await googleSignIn();
-    } catch (err) {
-      console.log(err);
-    }
-  };
   return (
-    <div className="h-14 flex items-center justify-between shadow-md px-5">
-      <div className="text-xs md:text-base font-poppins hover:text-rose-600 transition-all ease-in-out duration-200">
+    <div className="h-16 flex items-center justify-between border-[2px] px-5">
+      <div className="text-sm sm:text-base md:text-xl font-poppins hover:text-rose-600 transition-all ease-in-out duration-200">
         Giga Growth Ventures
       </div>
-      <div>
-        {user?.displayName ? (
-          <>
-            <button
-              className="text-xs md:text-base flex font-poppins hover:text-rose-600 transition-all ease-in-out duration-200"
-              onClick={handleLogout}
-            >
-              Log Out
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              className="text-xs md:text-base flex font-poppins hover:text-rose-600 transition-all ease-in-out duration-200"
-              onClick={handleGoogleSignIn}
-            >
-              Sign In
-            </button>
-          </>
-        )}
+      <div className="flex justify-center items-center gap-5 ">
+        <Sidebar />
       </div>
-      {/* <Sidebar /> */}
     </div>
   );
 };
