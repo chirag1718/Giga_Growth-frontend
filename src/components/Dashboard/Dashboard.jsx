22e3f34623286b8ac530eaf95f134cdddb383b9b
Chirag <chiragsonar1718@gmail.com>
2023-06-07 22:21:58 +0530

    feat: added fetched data in dashboard and fucntionality to send email.

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import GigaApi from "../../apis/GigaApi";
+import Email from "../General/Email";
 const Dashboard = () => {
+  const [selectedData, setSelectedData] = useState(null);
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await GigaApi.get("/dashboard");
+        setSelectedData(response.data);
+      };
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="my-10 ">
-        <div className="border-[1px] border-black w-[250px] h-[400px] md:w-[800px] md:h-[400px] lg:w-[800px] lg:h-[500px] xl:w-[1024px] shadow-md">
-          Hello I'm Dashboard Component
+        <div className="text-center text-lg mb-2">
+          <span>Stock: Relaince Industries 🚀</span>
+        </div>
+        <div className="border-[1px] border-gray-300 rounded-lg w-[250px] h-[300px] sm:w-[300px]  shadow-sm">
+          {selectedData && (
+            <div className="flex flex-col justify-center items-start">
+              {Object.entries(selectedData).map(([key, value]) => (
+                <div key={key} className="px-3 py-2 text-sm ">
+                  <span className="uppercase">{key} :</span>{" "}
+                  <span className="text-rose-500">₹ {value}</span>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <div>
-        <button className="text-xs md:text-base border-2 p-3 rounded-lg border-rose-500 active:scale-95">
-          Send Message
-        </button>
+        <Email />
       </div>
     </div>
   );
