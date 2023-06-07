2ea473fc507121e86f03d0d447ece1a9b169d237
Chirag <chiragsonar1718@gmail.com>
2023-06-07 10:40:53 +0530

    style: fixed styling and made responsive

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-5 justify-center items-center my-10 relative">
-        <div className="border-2 border-black w-[250px] h-[400px] md:w-[800px] md:h-[400px] lg:w-[800px] lg:h-[500px] xl:w-[1024px] shadow-md"></div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="my-10 ">
+        <div className="border-[1px] border-black w-[250px] h-[400px] md:w-[800px] md:h-[400px] lg:w-[800px] lg:h-[500px] xl:w-[1024px] shadow-md">
+          Hello I'm Dashboard Component
+        </div>
       </div>
       <div>
-        <button className="md:absolute md:bottom-3 md:right-5 border-2 p-3 rounded-lg border-rose-500 active:scale-95">
+        <button className="text-xs md:text-base border-2 p-3 rounded-lg border-rose-500 active:scale-95">
           Send Message
         </button>
       </div>
