08fbe6d03ab49d8f8652c24014537ca1e8bc5065
Chirag <chiragsonar1718@gmail.com>
2023-06-07 10:42:29 +0530

    Feat: added sign in & sign out functionality and removed sidebar.

--- src/components/General/Navigation.jsx
+++ src/components/General/Navigation.jsx
@@ -1,35 +1,49 @@
 import React from "react";
-import Sidebar from "./Sidebar";
-import { useNavigate } from "react-router-dom";
-
+import { UserAuth } from "../../context/AuthContext";
 const Navigation = () => {
-  const navigate = useNavigate();
-  const handleHome = () => {
-    navigate("/");
+  const { user, logOut } = UserAuth();
+  const { googleSignIn } = UserAuth();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (err) {
+      console.log(err);
+    }
   };
-  const handleDashboard = () => {
-    navigate("/dashboard");
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
-    <div className="h-14 sticky top-0 flex items-center justify-between z-99  bg-neutral-50 shadow-sm border-neutral-300- border-b-[1px]">
-      {window.location.pathname.includes("/dashboard") && (
-        <div
-          className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200"
-          onClick={handleDashboard}
-        >
-          Giga Growth Ventures
-        </div>
-      )}
-      {window.location.pathname.includes("/") &&
-        !window.location.pathname.includes("/dashboard") && (
-          <div
-            className="flex ml-3 md:ml-5 font-poppins cursor-pointer hover:text-rose-600 transition-all ease-in-out duration-200"
-            onClick={handleHome}
-          >
-            Giga Growth Ventures
-          </div>
+    <div className="h-14 flex items-center justify-between shadow-md px-5">
+      <div className="text-xs md:text-base font-poppins hover:text-rose-600 transition-all ease-in-out duration-200">
+        Giga Growth Ventures
+      </div>
+      <div>
+        {user?.displayName ? (
+          <>
+            <button
+              className="text-xs md:text-base flex font-poppins hover:text-rose-600 transition-all ease-in-out duration-200"
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="text-xs md:text-base flex font-poppins hover:text-rose-600 transition-all ease-in-out duration-200"
+              onClick={handleGoogleSignIn}
+            >
+              Sign In
+            </button>
+          </>
         )}
-      <Sidebar />
+      </div>
+      {/* <Sidebar /> */}
     </div>
   );
 };
