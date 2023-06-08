2f3da0c8e664d84bbd78d7f1db3e713828e9953d
Chirag <chiragsonar1718@gmail.com>
2023-06-08 09:03:21 +0530

    updated baseURL for deployment

--- src/apis/GigaApi.js
+++ src/apis/GigaApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8001/api/v1/",
+  // baseURL: "http://localhost:8001/api/v1/",
+  baseURL: "https://giga-growth-backend-git-main-chirag1718.vercel.app/api/v1",
 });
