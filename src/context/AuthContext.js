ac0caf347f092112bb92b1f2872290b9527a7db7
Chirag <chiragsonar1718@gmail.com>
2023-06-08 06:55:34 +0530

    cleaning

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -21,7 +21,7 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      console.log("user", currentUser);
+      // console.log("user", currentUser);s
     });
     return () => {
       unsubscribe();
